35acac60e5e3fedf5ee8125dd584fc6961014b5c
kkikorouge122-arch <kkiko.rouge122@gmail.com>
2026-05-26 20:32:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -193,12 +193,17 @@ with tabs[1]:
 
       # --- ONGLET 3 : ANALYSE ---
 with tabs[2]:
-    st.header("📊 Analyse des Performances")
+    st.subheader("📊 Audit Morphométrique")
     if not data.empty:
-        target = st.selectbox("Animal pour audit", data["ID"].unique(), key="ana_sel")
+        target = st.selectbox("Sélectionner le sujet d'étude", data["ID"].unique(), key="sel_audit")
         anim = data[data["ID"] == target].iloc[-1]
-        ic = anim['Poids'] / anim['LB'] if anim['LB'] > 0 else 0
-        st.metric("Indice Viande", f"{ic:.2f}")
+        try:
+        ic = float(anim['Poids']) / float(anim['LB']) if float(anim['LB']) > 0 else 0
+        ir = (float(anim['TP'])**2) / float(anim['HG']) if float(anim['HG']) > 0 else 0
+        c_an1, c_an2 = st.columns(2)
+        c_an1.metric("🥩 Indice de Compacité", f"{ic:.2f}")
+        c_an2.metric("🏗️ Indice de Robustesse", f"{ir:.2f}")
+       except Exception as e: st.error(f"Erreur indices : {e}")
         
     # --- ONGLET 4 : SANTÉ & SUIVI MÉDICAL ---
 with tabs[3]:
