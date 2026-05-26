d45f88462dfad8537be7ea1c51d3f206d793fa88
kkikorouge122-arch <kkiko.rouge122@gmail.com>
2026-05-26 20:06:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -282,7 +282,8 @@ with tabs[0]:
                         "LL": 5.0})
                         st.info("📈 Paramètres céphaliques et extrémités calculés.")
                         
-                    else:if target_sheep: 
+                    else:
+                     if target_sheep: 
                        st.error("❌ SUJET TROP ÉLOIGNÉ DU COLLIMATEUR : Recadrez la cible sous le viseur 🎯.")
                     else: st.error("❌ AUCUN INDIVIDU DÉTECTÉ : Stabilisez le smartphone à 2 mètres.")
                         st.divider()with st.form("form_final"):
